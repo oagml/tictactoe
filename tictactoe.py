e4097e7b929286679f86c7718760d54733b4d885
Os <oscar.agml@gmail.com>
2021-11-27 09:30:23 -0600

    Added a feature for telling wether a space has already been played or not

--- tictactoe.py
+++ tictactoe.py
@@ -59,7 +59,7 @@ def displayBoard(board):
 
 
 #Get input from player
-def playerInput():
+def playerInput(board):
 
     validInput = False #Flag used to control the validatio of user input
 
@@ -67,14 +67,50 @@ def playerInput():
 
         row, column = [int(x) for x in input("Enter the row and column values you want to play (separated by a comma): ").split(',')]
 
+        #Verify the user entered valid row and column positions
         if(row > 2 or row < 0 or column > 2 or column < 0):
             print("Error: You can only enter values from 0 - 2 \n")
-
+            validInput = False
         else:
             print(f"You entered the values: {row}, {column}")
             validInput = True
 
+        #Verify the slot chosen by the player is actually empty
+        if (board[row][column] == 0):
+            validInput = True
+        else: #if the slot already contains an X or O, the input is Invalid
+            print("This space has already been played, choose another one")
+            validInput = False
+
     return row, column
+
+
+#Create a function that determines the winner of the game
+#How does it work?
+#Define all the possible winning positions and scan the board to find if Xs or Os are filling
+#the winning positions
+#This is not an elegant solution in any way
+#Winning  row positions: [[(0,0),(0,1),(0,2)],[(1,0),(1,1),(1,2)],[(2,0),(2,1),(2,2)]]
+#Winning  column  positions: [[(0,0),(1,0),(2,0)],[(0,1),(1,1),(2,1)],[(0,2),(1,2),(2,2)]]
+#Winning  diagonal positions: [[(0,0),(1,1),(2,2)],[(2,0),(1,1),(0,2)]]
+
+def findWinner (board):
+    
+    winningRows = [[(0,0),(0,1),(0,2)],[(1,0),(1,1),(1,2)],[(2,0),(2,1),(2,2)]]
+    winningCols = [[(0,0),(1,0),(2,0)],[(0,1),(1,1),(2,1)],[(0,2),(1,2),(2,2)]]
+    winningDiags = [[(0,0),(1,1),(2,2)],[(2,0),(1,1),(0,2)]]
+
+    for i in winningRows:
+        for j in winningRows[i]:
+            
+
+
+
+
+
+
+
+
 
 #Flag for controlling the end of the game
 gameOver = False
@@ -96,7 +132,7 @@ while (gameOver == False):
     print(f"It's {boardValues[currentPlayer]}'s turn!")
     
     #Player enters the slot he/she wants to play
-    cRow, cCol = playerInput()
+    cRow, cCol = playerInput(board)
 
     #Slot is updated with the corresponding symbol
     if (currentPlayer == 1): #If it's X's turn 
@@ -114,9 +150,6 @@ while (gameOver == False):
         currentPlayer = 2
     else:
         currentPlayer = 1
-
-
-
 
 
 
